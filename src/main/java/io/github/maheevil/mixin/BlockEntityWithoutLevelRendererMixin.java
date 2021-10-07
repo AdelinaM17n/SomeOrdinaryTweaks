ffc88c49ba5daf6560ae4169afd06321c0d7f7e7
Maheesha <maheeshasithum@gmail.com>
2021-10-07 12:36:01 +0530

    ;-;

--- src/main/java/io/github/maheevil/mixin/BlockEntityWithoutLevelRendererMixin.java
+++ src/main/java/io/github/maheevil/mixin/BlockEntityWithoutLevelRendererMixin.java
@@ -1,23 +1,22 @@
 package io.github.maheevil.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.item.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BlockEntityWithoutLevelRenderer.class)
-public class BlockEntityWithoutLevelRendererMixin {
-    @Shadow @Final private BlockEntityRenderDispatcher blockEntityRenderDispatcher;
 
+@Mixin(BlockEntityWithoutLevelRenderer.class)
+@Environment(EnvType.CLIENT)
+public abstract class BlockEntityWithoutLevelRendererMixin {
     @Inject(
             method = "renderByItem",
             at = @At(
@@ -28,8 +27,15 @@ public class BlockEntityWithoutLevelRendererMixin {
             cancellable = true
     )
     public void rendermixin(ItemStack itemStack, ItemTransforms.TransformType transformType, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, CallbackInfo ci){
-        if(transformType == ItemTransforms.TransformType.FIRST_PERSON_LEFT_HAND && this.blockEntityRenderDispatcher.level.players().get(0).getMainHandItem() != null &&!this.blockEntityRenderDispatcher.level.players().get(0).getMainHandItem().is(Items.STONE_SWORD)) {
+        assert Minecraft.getInstance().player != null;
+        ItemStack MainHandItemStack = Minecraft.getInstance().player.getMainHandItem();
+
+        if(
+                transformType == ItemTransforms.TransformType.FIRST_PERSON_LEFT_HAND && MainHandItemStack != null
+                && !(MainHandItemStack.getItem() instanceof AxeItem || MainHandItemStack.getItem() instanceof SwordItem
+                || MainHandItemStack.getItem() instanceof ProjectileWeaponItem || MainHandItemStack.getItem() instanceof TridentItem)
+        )
             ci.cancel();
-        }
+
     }
 }
