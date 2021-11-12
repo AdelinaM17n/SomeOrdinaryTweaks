f62578b35d2b118b5933b827f86ea3a16cc0fe8c
Maheesha <maheeshasithum@gmail.com>
2021-11-12 15:09:14 +0530

    testing if the github action still fails

--- src/main/java/io/github/maheevil/SomeOrdinaryTweaksMod.java
+++ src/main/java/io/github/maheevil/SomeOrdinaryTweaksMod.java
@@ -5,5 +5,6 @@ import net.fabricmc.api.ModInitializer;
 public class SomeOrdinaryTweaksMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
+		System.out.println("Haai");
 	}
 }
