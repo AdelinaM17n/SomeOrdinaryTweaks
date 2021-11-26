14ebd7deca78ab9a70df8433ab8eb7251c65098a
Maheesha <maheeshasithum@gmail.com>
2021-11-26 09:42:38 +0530

    Revert "one person said this looks better"
    This reverts commit b5e87b5dbca2af3c9740f1ba6e4fa1c2867d816b.

--- src/main/java/io/github/maheevil/ordinarytweaks/config/ModConfig.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/config/ModConfig.java
@@ -10,27 +10,16 @@ public class ModConfig implements ConfigData {
      * the values below are default values
      */
     @ConfigEntry.Gui.PrefixText
-    @ConfigEntry.Category("Render/Gui Tweaks")
     public boolean invisibleShield = false;
-    @ConfigEntry.Category("Render/Gui Tweaks")
     public boolean invisibleTotem = false;
-    @ConfigEntry.Category("Render/Gui Tweaks")
     public boolean betterHorseHUD = false;
-
     @ConfigEntry.Gui.PrefixText
-    @ConfigEntry.Category("Toast Control")
     public boolean hideTutorialToasts = true;
-    @ConfigEntry.Category("Toast Control")
     public boolean hideAdvancementToasts = false;
-    @ConfigEntry.Category("Toast Control")
     public boolean hideRecipeUnlockToasts = false;
-
     @ConfigEntry.Gui.PrefixText
-    @ConfigEntry.Category("General Tweaks")
     public boolean deathCordsClipBoardButton = true;
-    @ConfigEntry.Category("General Tweaks")
     public boolean disablePortalGUIClosing = false;
-    @ConfigEntry.Category("General Tweaks")
     public boolean skipResourcePackDownload = false;
 
 
