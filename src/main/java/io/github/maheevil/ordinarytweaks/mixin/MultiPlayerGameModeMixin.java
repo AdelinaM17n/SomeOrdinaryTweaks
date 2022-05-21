6d7dc073a39c9d019209a9144d66a2eea1329a2c
Maheesha <maheeshasithum@gmail.com>
2022-05-21 17:05:08 +0530

    Fully working

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/MultiPlayerGameModeMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/MultiPlayerGameModeMixin.java
@@ -1,46 +1,40 @@
 package io.github.maheevil.ordinarytweaks.mixin;
 
+import io.github.maheevil.ordinarytweaks.SomeOrdinaryTweaksMod;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.phys.BlockHitResult;
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MultiPlayerGameMode.class)
 public abstract class MultiPlayerGameModeMixin {
     @Shadow public abstract InteractionResult useItem(Player player, InteractionHand interactionHand);
 
-    /*@Inject(
-            method = "performUseItemOn",
-            at = @At(
-                    value = "FIELD",
-                    target = "net/minecraft/client/multiplayer/MultiPlayerGameMode.minecraft:Lnet/minecraft/client/Minecraft;",
-                    shift = At.Shift.BEFORE,
-                    ordinal = 0
-            ),
+    @Inject(
+            method = "method_41933(Lorg/apache/commons/lang3/mutable/MutableObject;Lnet/minecraft/client/player/LocalPlayer;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;I)Lnet/minecraft/network/protocol/Packet;",
+            at = @At("HEAD"),
             cancellable = true
     )
-    public void useItemon(LocalPlayer player, InteractionHand hand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir){
-        Item itemInHand = hand == InteractionHand.MAIN_HAND ? player.getMainHandItem().getItem() : player.getOffhandItem().getItem();
+    public void injectTest(MutableObject mutableObject, LocalPlayer localPlayer, InteractionHand interactionHand, BlockHitResult blockHitResult, int i, CallbackInfoReturnable<Packet> cir){
+        Item itemInHand = localPlayer.getItemInHand(interactionHand).getItem();
         boolean isValidItem = itemInHand instanceof BlockItem && itemInHand.isEdible();
-        if(SomeOrdinaryTweaksMod.config.doNotPlantEdiblesIfHungry
-                && player.getFoodData().needsFood()
-                && isValidItem)
-        {
-            cir.setReturnValue(this.useItem(player,hand));
+        if(SomeOrdinaryTweaksMod.config.doNotPlantEdiblesIfHungry && localPlayer.getFoodData().needsFood() && isValidItem) {
+            mutableObject.setValue(useItem(localPlayer, interactionHand));
+            cir.setReturnValue(new ServerboundUseItemPacket(interactionHand, i));
             cir.cancel();
         }
-    }*/
-    @ModifyVariable(
-            method = "performUseItemOn",
-            at = @At("STORE"),
-            index = 7,
-            ordinal = 1
-    )
-    private boolean bruh(boolean value){
-        return true;
     }
+
 }
