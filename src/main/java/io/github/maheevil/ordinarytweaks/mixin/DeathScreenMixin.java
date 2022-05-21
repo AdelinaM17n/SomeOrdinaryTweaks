b244db0d5210f14306f500cc1014a36243aa0d81
Maheesha <maheeshasithum@gmail.com>
2022-05-21 15:39:22 +0530

    MultiPlayerGameModeMixin No worky

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/DeathScreenMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/DeathScreenMixin.java
@@ -6,7 +6,7 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
+//import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -56,7 +56,7 @@ public abstract class DeathScreenMixin extends Screen {
             assert minecraft != null;
             assert minecraft.player != null;
             String xyz =  minecraft.player.blockPosition().getX() + " / " + minecraft.player.blockPosition().getY() + " / " + minecraft.player.blockPosition().getZ();
-            this.exitButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 120, 200, 20, new TranslatableComponent("Copy Location To Clipboard"), (buttonx) -> {
+            this.exitButtons.add(this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 120, 200, 20, Component.translatable("Copy Location To Clipboard"), (buttonx) -> {
                 this.minecraft.keyboardHandler.setClipboard(xyz);
             })));
         }
--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/MultiPlayerGameModeMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/MultiPlayerGameModeMixin.java
@@ -1,45 +1,46 @@
 package io.github.maheevil.ordinarytweaks.mixin;
 
-import io.github.maheevil.ordinarytweaks.SomeOrdinaryTweaksMod;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(MultiPlayerGameMode.class)
 public abstract class MultiPlayerGameModeMixin {
-    @Shadow public abstract InteractionResult useItem(Player player, Level level, InteractionHand hand);
+    @Shadow public abstract InteractionResult useItem(Player player, InteractionHand interactionHand);
 
-    @Inject(
-            method = "useItemOn",
+    /*@Inject(
+            method = "performUseItemOn",
             at = @At(
-                    value = "INVOKE",
-                    target = "net/minecraft/client/multiplayer/ClientLevel.getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;",
+                    value = "FIELD",
+                    target = "net/minecraft/client/multiplayer/MultiPlayerGameMode.minecraft:Lnet/minecraft/client/Minecraft;",
                     shift = At.Shift.BEFORE,
                     ordinal = 0
             ),
             cancellable = true
     )
-    public void useItemon(LocalPlayer player, ClientLevel level, InteractionHand hand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir){
+    public void useItemon(LocalPlayer player, InteractionHand hand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir){
         Item itemInHand = hand == InteractionHand.MAIN_HAND ? player.getMainHandItem().getItem() : player.getOffhandItem().getItem();
         boolean isValidItem = itemInHand instanceof BlockItem && itemInHand.isEdible();
         if(SomeOrdinaryTweaksMod.config.doNotPlantEdiblesIfHungry
                 && player.getFoodData().needsFood()
                 && isValidItem)
         {
-            cir.setReturnValue(this.useItem(player,level,hand));
+            cir.setReturnValue(this.useItem(player,hand));
             cir.cancel();
         }
+    }*/
+    @ModifyVariable(
+            method = "performUseItemOn",
+            at = @At("STORE"),
+            index = 7,
+            ordinal = 1
+    )
+    private boolean bruh(boolean value){
+        return true;
     }
 }
