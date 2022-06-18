6f131b52ca595d222cd82629a2e3e9e9a755014c
Maheesha <maheeshasithum@gmail.com>
2022-06-18 16:35:47 +0530

    Bug fix with warden and darkness

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/LightTextureMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/LightTextureMixin.java
@@ -1,10 +1,13 @@
 package io.github.maheevil.ordinarytweaks.mixin;
 
 import io.github.maheevil.ordinarytweaks.SomeOrdinaryTweaksMod;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.world.effect.MobEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LightTexture.class)
 public abstract class LightTextureMixin {
@@ -13,9 +16,19 @@ public abstract class LightTextureMixin {
           at = @At(value = "STORE"),
           index = 21,
           ordinal = 14
-          //print = true
   )
   public float br(float value){
       return SomeOrdinaryTweaksMod.config.fullBright ? 100 : value;
+  }
+
+  @Redirect(
+          method = "getDarknessGamma",
+          at = @At(
+                  value = "INVOKE",
+                  target = "net/minecraft/client/player/LocalPlayer.hasEffect (Lnet/minecraft/world/effect/MobEffect;)Z"
+          )
+  )
+  public boolean darknessInject(LocalPlayer instance, MobEffect mobEffect){
+      return !SomeOrdinaryTweaksMod.config.fullBright && instance.hasEffect(mobEffect);
   }
 }
