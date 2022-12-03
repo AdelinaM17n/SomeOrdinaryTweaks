be7109a0ae49bbe15353550856e1222c11d09c9d
Maheesha <maheeshasithum@gmail.com>
2022-12-03 10:55:19 +0530

    Fullbright Fixed
    Signed-off-by: Maheesha <maheeshasithum@gmail.com>

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/LightTextureMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/LightTextureMixin.java
@@ -14,8 +14,9 @@ public abstract class LightTextureMixin {
   @ModifyVariable(
           method = "updateLightTexture",
           at = @At(value = "STORE"),
-          index = 21,
-          ordinal = 14
+          index = 8,
+          ordinal = 6,
+          print = true
   )
   public float br(float value){
       return SomeOrdinaryTweaksMod.config.fullBright ? 100 : value;
