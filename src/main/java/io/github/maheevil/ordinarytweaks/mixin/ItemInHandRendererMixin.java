4b0a21fd664a8adfc040c64eef72b517f5c1a1af
Maheesha Adelina <MaheeshaAdelina@gmail.com>
2024-12-12 23:39:27 +0530

    Done. I'm an insane girl.

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/ItemInHandRendererMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/ItemInHandRendererMixin.java
@@ -3,12 +3,16 @@ package io.github.maheevil.ordinarytweaks.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.maheevil.ordinarytweaks.SomeOrdinaryTweaksMod;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.world.item.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -41,6 +45,39 @@ public class ItemInHandRendererMixin {
                     && itemDisplayContext.getId() % 2 == Minecraft.getInstance().player.getMainArm().getId()
                     && itemStack.is(Items.TOTEM_OF_UNDYING)
             ) ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "renderItem",
+            at = @At(
+                    value = "HEAD"
+            ),
+            cancellable = true
+    )
+    private void renderByItemMixin(
+            LivingEntity livingEntity, ItemStack itemStack, ItemDisplayContext itemDisplayContext, boolean bl, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci
+    ){
+        /*
+         * ItemDisplayContext.getId()'s first person ids (in decimal) are 3 (left) and 4 (right)
+         * The player's main arm's ID is either 0 (left) or 1 (right)
+         *
+         * So when ItemDisplayContext's ID's remainder is taken after dividing by 2, its left and right should
+         * mismatch with the main arm's left and right arms
+         * If they are equal that means it's rendering the offhand item, And that is cancelled if the item matches
+         */
+        if(SomeOrdinaryTweaksMod.config.invisibleShield){
+            assert Minecraft.getInstance().player != null;
+            LocalPlayer localPlayer = Minecraft.getInstance().player;
+            ItemStack mainHandItemStack = localPlayer.getMainHandItem();
+
+            if(itemDisplayContext.firstPerson() && itemDisplayContext.getId() % 2 == localPlayer.getMainArm().getId()){
+                if(!(mainHandItemStack.getItem() instanceof AxeItem
+                        || mainHandItemStack.getItem() instanceof SwordItem
+                        || mainHandItemStack.getItem() instanceof ProjectileWeaponItem
+                        || mainHandItemStack.getItem() instanceof TridentItem)
+                ) ci.cancel();
+            }
         }
     }
 }
