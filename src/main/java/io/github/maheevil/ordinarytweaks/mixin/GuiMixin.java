4ea1758f658a7030f37e94d18954f9d9636aa0b1
Maheesha Adelina <MaheeshaAdelina@gmail.com>
2024-12-12 22:59:12 +0530

    Literally reimplemented half the features because I am supergirl like that, ok maybe not

--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/GuiMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/GuiMixin.java
@@ -1,31 +1,39 @@
 package io.github.maheevil.ordinarytweaks.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import io.github.maheevil.ordinarytweaks.SomeOrdinaryTweaksMod;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PlayerRideableJumping;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.scores.Objective;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Gui.class)
 @Environment(EnvType.CLIENT)
 public abstract class GuiMixin {
-    /**@Shadow protected abstract int getVisibleVehicleHeartRows(int i);
-    @Shadow protected abstract int getVehicleMaxHearts(LivingEntity livingEntity);
-    @Shadow protected abstract LivingEntity getPlayerVehicleWithHealth();
-    @Shadow private int screenHeight;
     @Shadow @Final private Minecraft minecraft;
+
+    @Shadow protected abstract void renderExperienceBar(GuiGraphics guiGraphics, int i);
+
+    @Shadow protected abstract int getVehicleMaxHearts(@Nullable LivingEntity livingEntity);
+
+    @Shadow @Nullable protected abstract LivingEntity getPlayerVehicleWithHealth();
+
+    @Shadow public abstract void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker);
 
     @SuppressWarnings("ConstantConditions")
     @ModifyVariable(
@@ -34,49 +42,86 @@ public abstract class GuiMixin {
             index = 5,
             ordinal = 2
     )
-    private int modify_int_k(int i){
-        return (SomeOrdinaryTweaksMod.config.betterHorseHUD && this.minecraft.gameMode.getPlayerMode() != GameType.CREATIVE) ? this.screenHeight - 49 :i;
+    private int modify_int_k(int i, GuiGraphics guiGraphics){
+        return (SomeOrdinaryTweaksMod.config.betterHorseHUD && this.minecraft.gameMode.getPlayerMode() != GameType.CREATIVE) ? guiGraphics.guiHeight() - 49 : i;
     }
 
-    @ModifyVariable(
+
+    @ModifyExpressionValue(
             method = "renderPlayerHealth",
-            at = @At("STORE"),
-            index = 22,
-            ordinal = 13
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getVehicleMaxHearts(Lnet/minecraft/world/entity/LivingEntity;)I")
     )
     private int modify_int_y(int in){
         return SomeOrdinaryTweaksMod.config.betterHorseHUD ? 0 : in;
     }
 
-    @ModifyArg(
-            method = "renderPlayerHealth",
-            at = @At(value ="INVOKE", target = "net/minecraft/client/gui/Gui.getVisibleVehicleHeartRows(I)I", ordinal = 0),
-            index = 0
-    )
-    private int modify_methodArg_vehicleMaxHearts(int original){
-        return SomeOrdinaryTweaksMod.config.betterHorseHUD
-                ? this.getVisibleVehicleHeartRows(this.getVehicleMaxHearts(this.getPlayerVehicleWithHealth())) : original;
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    @Redirect(
-            method = "render",
+    @Inject(
+            method = "renderHotbarAndDecorations",
             at = @At(
                     value = "INVOKE",
-                    target = "net/minecraft/client/player/LocalPlayer.jumpableVehicle()Lnet/minecraft/world/entity/PlayerRideableJumping;",
-                    ordinal = 0
-            )
-    )
-    private PlayerRideableJumping redirect_isRidingJumpable_IF(LocalPlayer instance){
-        return SomeOrdinaryTweaksMod.config.betterHorseHUD && this.minecraft.gameMode.getPlayerMode() != GameType.CREATIVE
-                ? this.minecraft.options.keyJump.isDown()
-                    ? instance.jumpableVehicle() : null
-                : instance.jumpableVehicle();
+                    target = "Lnet/minecraft/client/gui/Gui;renderJumpMeter(Lnet/minecraft/world/entity/PlayerRideableJumping;Lnet/minecraft/client/gui/GuiGraphics;I)V",
+                    shift = At.Shift.AFTER
+            ),
+            locals = LocalCapture.CAPTURE_FAILEXCEPTION
+    )private void renderXpPostJumpMeter(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci, int i, PlayerRideableJumping playerRideableJumping){
+        if(SomeOrdinaryTweaksMod.config.betterHorseHUD && this.minecraft.gameMode.hasExperience() && !this.minecraft.options.keyJump.isDown()){
+            this.renderExperienceBar(guiGraphics,i);
+        }
     }
 
-    @SuppressWarnings("all") // MCDEV STOP TRYING TO GASLIGHT ME
+    @WrapWithCondition(
+            method = "renderHotbarAndDecorations",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/Gui;renderJumpMeter(Lnet/minecraft/world/entity/PlayerRideableJumping;Lnet/minecraft/client/gui/GuiGraphics;I)V"
+            )
+    )
+    private boolean wrapRenderJumpMeter(Gui instance, PlayerRideableJumping playerRideableJumping, GuiGraphics guiGraphics, int i){
+        return !SomeOrdinaryTweaksMod.config.betterHorseHUD || !this.minecraft.gameMode.hasExperience() || this.minecraft.options.keyJump.isDown();
+    }
+
+    @ModifyArg(
+            method = "renderPlayerHealth",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/Gui;renderAirBubbles(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/entity/player/Player;III)V"
+            ),
+            index = 2
+    )private int giveCorrectIValToAirBubbles(int i){
+        if(SomeOrdinaryTweaksMod.config.betterHorseHUD &&  i != 0)
+            return this.getVehicleMaxHearts(this.getPlayerVehicleWithHealth());
+        else
+            return i;
+    }
+
+    @ModifyArg(
+            method = "renderPlayerHealth",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/Gui;renderAirBubbles(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/entity/player/Player;III)V"
+            ),
+            index = 3
+    )private int giveCorrectRValToAirBubbles(int i){
+        if(SomeOrdinaryTweaksMod.config.betterHorseHUD &&  i != 0)
+            return i - 10;
+        else
+            return i;
+    }
+
+    @ModifyExpressionValue(
+            method = "renderExperienceLevel",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;isExperienceBarVisible()Z")
+    )
+    private boolean makeXpLevelVisible(boolean original){
+        if(SomeOrdinaryTweaksMod.config.betterHorseHUD && this.minecraft.gameMode.hasExperience()) {
+            return true;
+        }
+        return original;
+    }
+
+    //@SuppressWarnings("all") // MCDEV STOP TRYING TO GASLIGHT ME
     @ModifyVariable(
-            method = "renderHotbar",
+            method = "renderItemHotbar",
             at = @At("STORE"),
             index =  5,
             ordinal = 0
@@ -96,5 +141,5 @@ public abstract class GuiMixin {
     )
     private void inject_scoreboard$ordinarytweaks(GuiGraphics guiGraphics, Objective objective, CallbackInfo ci){
         if(SomeOrdinaryTweaksMod.config.disableScoreboard) ci.cancel();
-    } **/
+    }
 }
--- src/main/java/io/github/maheevil/ordinarytweaks/mixin/TextDisplayRendererMixin.java
+++ src/main/java/io/github/maheevil/ordinarytweaks/mixin/TextDisplayRendererMixin.java
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DisplayRenderer.TextDisplayRenderer.class)
 public class TextDisplayRendererMixin {
-    /**@Redirect(
-            method = "renderInner(Lnet/minecraft/world/entity/Display$TextDisplay;Lnet/minecraft/world/entity/Display$TextDisplay$TextRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IF)V",
+    @Redirect(
+            method = "renderInner(Lnet/minecraft/client/renderer/entity/state/TextDisplayEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IF)V",
             at = @At(value = "INVOKE", target = "net/minecraft/client/Options.getBackgroundOpacity (F)F")
     )
     private float redirectBackgroundOpacity$ordinarytweaks(Options instance, float opacity){
@@ -22,5 +22,5 @@ public class TextDisplayRendererMixin {
             case _75_PERCENT -> 0.75F;
             case _100_PERCENT -> 1.0F;
         };
-    } **/
+    }
 }
